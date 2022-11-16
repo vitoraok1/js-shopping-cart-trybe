9b3a86467cd4056f8d400b7a24cc58a0b6024c3f
vitoraok1 <vitoraoki17@gmail.com>
2022-11-16 17:02:39 -0300

    requirement 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createCartProductElement, createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
+const productsArea = document.querySelector('.products');
+
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+async function addItems() {
+  const products = await fetchProductsList('computador');
+  products.map((product) => productsArea.appendChild(createProductElement(product)));
+}
+
+window.onload = () => {
+  addItems();
+};
