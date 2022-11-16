d482f58fff0dabccd4889ef44bebace64e9c4d09
vitoraok1 <vitoraoki17@gmail.com>
2022-11-16 17:23:57 -0300

    requirement 4

--- src/main.js
+++ src/main.js
@@ -7,9 +7,22 @@ const productsArea = document.querySelector('.products');
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
+function addLoading() {
+  const h2 = document.createElement('h2');
+  h2.className = 'loading';
+  h2.innerHTML = 'carregando...';
+  productsArea.appendChild(h2);
+}
+
+function hideLoading() {
+  document.querySelector('.loading').remove();
+}
+
 async function addItems() {
+  addLoading();
   const products = await fetchProductsList('computador');
   products.map((product) => productsArea.appendChild(createProductElement(product)));
+  hideLoading();
 }
 
 window.onload = () => {
